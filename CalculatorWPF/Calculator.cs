72bd6500cb251ed7e96603b9d64869ef06ba1671
Danila <danchik.b@mail.ru>
2024-01-31 15:09:41 +0300

    HW float

--- CalculatorWPF/Calculator.cs
+++ CalculatorWPF/Calculator.cs
@@ -1,70 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculatorWPF
+﻿namespace CalculatorWPF
 {
-  class CalculatorArgs : EventArgs
+  class Calculator : CalculatorExeptions
   {
-    public int answer = 0;
-  }
-  internal class Calculator
-  {
-    public event EventHandler<CalculatorArgs> Result;
 
-    public int result { get; private set; } = 0;
-    Stack<int> results = new Stack<int>();
-
-
-    private void Calculation()
-    {
-      Result?.Invoke(this, new CalculatorArgs() { answer = result });
-    }
-
-    public void Add(int value)
-    {
-      results.Push(value);
-      result += value;
-      Calculation();
-    }
-
-    public void Sub(int value)
-    {
-      results.Push(value);
-      result -= value;
-      Calculation();
-    }
-
-    public void Mult(int value)
-    {
-      results.Push(value);
-      result *= value;
-      Calculation();
-    }
-
-    public void Div(int value)
-    {
-      results.Push(value);
-      result /= value;
-      Calculation();
-    }
-
-    public void Cancel()
-    {
-      if (results.Count > 0)
-      {
-        result = results.Pop();
-        Calculation();
-      }
-    }
-
-    public void C()
-    {
-      results.Clear();
-      result = 0;
-      Calculation();
-    }
   }
 }
--- CalculatorWPF/MainWindow.xaml.cs
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalculatorWPF
 {
@@ -27,7 +15,6 @@ namespace CalculatorWPF
       InitializeComponent();
       calculator = new Calculator();
       calculator.Result += Calculator_Result;
-
     }
 
     private void Calculator_Result(object sender, CalculatorArgs e)
@@ -40,9 +27,10 @@ namespace CalculatorWPF
       bool parse = int.TryParse(InputText.Text, out int value);
       var name = (e.Source as FrameworkElement).Name;
 
-      if (InputText.Text == "" || name == "C")
+      if (InputText.Text == "" || name == "Clear")
       {
-        calculator.C();
+        calculator.Clear();
+        InputText.Text = String.Empty;
       }
       else
       {
@@ -70,12 +58,21 @@ namespace CalculatorWPF
             calculator.Div(value);
             break;
 
+          case "Cancel":
+            calculator.Cancel();
+            break;
+
           default:
             MessageBox.Show("Ошибка нажатия кнопки!");
             InputText.Text = string.Empty;
             break;
         }
       }
+    }
+
+    private void Window_Closed(object sender, EventArgs e)
+    {
+      calculator.Result -= Calculator_Result;
     }
   }
 }
